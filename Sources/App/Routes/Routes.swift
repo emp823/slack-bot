1c254e71c66a0f152e71680faed57cd1377fc5de
Erik Pearson <emp823@gmail.com>
2017-10-08 02:44:08 -0400

    Add apod route

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -1,25 +1,24 @@
 import Vapor
 
 extension Droplet {
-    func setupRoutes() throws {
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
+  func setupRoutes() throws {
+    get("hello") { req in
+      var json = JSON()
+      try json.set("hello", "world")
+      return json
     }
+
+    get("apod") { req in
+      let res = try self.client.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      return try JSON(node: [
+        "text": "NASA Pic of the Day",
+        "attachments": [
+          "image_url": res.data["url"],
+          "title": res.data["title"]
+        ]
+      ])
+    }
+
+    try resource("posts", PostController.self)
+  }
 }
