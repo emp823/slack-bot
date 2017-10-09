65ba0f8a410be87638465c57c217afdabf0a0262
Erik Pearson <emp823@gmail.com>
2017-10-08 21:09:28 -0400

    Put in array

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -11,21 +11,23 @@ extension Droplet {
     get("apod") { req in
       return try self.getNasaData()
     }
-    
+
     post("apod") { req in
       return try self.getNasaData()
     }
 
     try resource("posts", PostController.self)
   }
-    
+
     private func getNasaData() throws -> JSON {
       let res = try self.client.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
       return try JSON(node: [
         "text": "NASA Pic of the Day",
         "attachments": [
+          [
             "image_url": res.data["url"],
             "title": res.data["title"]
+          ]
         ]
       ])
     }
