0d072a0762619949ee4526d6201588ad098e939c
Erik Pearson <emp823@gmail.com>
2017-10-08 20:48:42 -0400

    Add post for slack

--- Sources/App/Routes/Routes.swift
+++ Sources/App/Routes/Routes.swift
@@ -9,16 +9,24 @@ extension Droplet {
     }
 
     get("apod") { req in
-      let res = try self.client.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
-      return try JSON(node: [
-        "text": "NASA Pic of the Day",
-        "attachments": [
-          "image_url": res.data["url"],
-          "title": res.data["title"]
-        ]
-      ])
+      return try self.getNasaData()
+    }
+    
+    post("apod") { req in
+      return try self.getNasaData()
     }
 
     try resource("posts", PostController.self)
   }
+    
+    private func getNasaData() throws -> JSON {
+      let res = try self.client.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+      return try JSON(node: [
+        "text": "NASA Pic of the Day",
+        "attachments": [
+            "image_url": res.data["url"],
+            "title": res.data["title"]
+        ]
+      ])
+    }
 }
